4cb52f9af08c44a27e185f0580c6e2a88d692b42
Anatoli Shchatko <Anatoli_Shchatko@STYLESOFT.BY>
2022-02-18 23:26:53 +0300

    test for accept allert

---
+++ lesson2_3_step4.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import time
+import math
+
+
+def calc(n1):
+  return str(math.log(abs(12*math.sin(n1))))
+
+try: 
+    link = "http://suninjuly.github.io/alert_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+    
+    confirm=browser.switch_to.alert
+    confirm.accept()
+    
+      
+    
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    n1=int(x)
+    y = calc(n1)
+    
+    input1=browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click() 
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
